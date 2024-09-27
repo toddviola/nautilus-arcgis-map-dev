41f0659193099f30e27489059ac5ff05d7f67871
Ben Kane <benkane11@gmail.com>
2024-09-27 16:13:11 -0700

    Attach source layer to graphics layer returned by asGraphicsLayer utility function so that the source data can be interacted with, updated, etc as needed

--- web/modules/custom/nautilus_arcgis_block/assets/util/layers.js
+++ web/modules/custom/nautilus_arcgis_block/assets/util/layers.js
@@ -7,7 +7,7 @@ export async function asGraphicsLayer(layer, options = {}) {
     let query = layer.createQuery();
     query.where = layer.definitionExpression;
     const {features, fields, geometryType, spatialReference} = await layer.queryFeatures(query);
-    const graphics = features.map(({attributes, geometry}) => new Graphic({attributes: attributes, geometry}));
+    const graphics = features.map(({attributes, geometry}) => new Graphic({attributes, geometry}));
     const graphicsLayer = new FeatureLayer({
         fields,
         geometryType,
@@ -18,5 +18,6 @@ export async function asGraphicsLayer(layer, options = {}) {
         title: layer.title.replace('OET Cruises master CCOM configured - ', ''),
         ...options,
     });
+    graphicsLayer.sourceLayer = layer; // store the source layer so the graphicsLayer can be updated as needed
     return graphicsLayer;
 }
